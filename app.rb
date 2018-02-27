7d89381d3754d977ed5a9da2eac062067213e6af
Chase Bowden and Hannah Lee <hannah@>
2018-02-26 16:06:05 -0800

    add images, styling, division and employee view files.

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@ require('sinatra')
 require('sinatra/reloader')
 require('sinatra/activerecord')
 also_reload("lib/**/*.rb")
-require('./lib/employee')
+require('./lib/division')
 require('./lib/project')
+require('./lib/employee')
 require('rspec')
 require('pg')
 require('pry')
+
+get('/') do
+  @divisions = Division.all
+  @employees = Employee.all
+  erb(:index)
+end
+
+get('/divisions') do
+  erb(:divisions)
+end
+
+post('/divisions') do
+  @division = Division.create(name: params[:name])
+  @divisions = Division.all
+  binding.pry
+  erb(:divisions)
+end
